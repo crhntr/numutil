e3f1a57f57a3905ac1cd0f5a478a02fff5d01baf
crhntr <hunter.christopher@icloud.com>
2018-04-06 19:41:11 -0700

    Fixed Misspelling and Reorganized Code

--- words.go
+++ words.go
@@ -1,61 +1,29 @@
 package numutil
 
+import "strings"
+
 func ToWords(n int) string {
-	if n < 0 {
+	switch {
+	case n < 0:
 		return "negative " + ToWords(-n)
-	}
-	if n < 20 {
+	case n < 20:
 		return upToNineteen[n]
+	case n < 100:
+		return strings.TrimSpace(tens[n/10-2] + " " + upToNineteen[n-(n/10*10)])
+	case n < 1000:
+		return strings.TrimSpace(upToNineteen[n/100] + " hundred " + ToWords(n-(n/100*100)))
+	case n < 1000000:
+		return strings.TrimSpace(ToWords(n/1000) + " thousand " + ToWords(n%1000))
+	case n < 1000000000:
+		return strings.TrimSpace(ToWords(n/1000000) + " billion " + ToWords(n%1000000))
+	default:
+		return strings.TrimSpace(ToWords(n/1000000000) + " trillion " + ToWords(n%1000000000))
 	}
-	if n < 100 {
-		lowOrder := upToNineteen[:10][n-(n/10*10)]
-		if lowOrder != "" {
-			lowOrder = " " + lowOrder
-		}
-		return tens[n/10-2] + lowOrder
-	}
-	if n < 1000 {
-		return upToNineteen[:10][n/100] + " hundred " + ToWords(n-(n/100*100))
-	}
-	if n < 1000000 {
-		return ToWords(n/1000) + " thosand " + ToWords(n%1000)
-	}
-	if n < 1000000000 {
-		return ToWords(n/1000000) + " billion " + ToWords(n%1000000)
-	}
-	return ToWords(n/1000000000) + " trillion " + ToWords(n%1000000000)
 }
 
 var upToNineteen = [...]string{
-	"", // zero
-	"one",
-	"two",
-	"three",
-	"four",
-	"five",
-	"six",
-	"seven",
-	"eight",
-	"nine",
-	"ten",
-	"eleven",
-	"twelve",
-	"thirteen",
-	"fourteen",
-	"fifteen",
-	"sixteen",
-	"seventeen",
-	"eighteen",
-	"nineteen",
+	"" /*"zero"*/, "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+	"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
 }
 
-var tens = [...]string{
-	"twenty",
-	"thirty",
-	"forty",
-	"fifty",
-	"sixty",
-	"seventy",
-	"eighty",
-	"ninety",
-}
+var tens = [...]string{"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}
--- words_test.go
+++ words_test.go
@@ -1,29 +1,15 @@
 package numutil_test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/crhntr/numutil"
 )
 
 func TestToWords(t *testing.T) {
-	for n := -19; n < 100; n++ {
-		t.Log(numutil.ToWords(n))
-	}
-
-	for n := 100; n < 1000; n += 55 {
-		t.Log(numutil.ToWords(n))
-	}
-
-	for n := 1000; n < 1000000; n += 77654 {
-		t.Log(numutil.ToWords(n))
-	}
-
-	for n := 1000000; n < 1000000000; n += 7747654 {
-		t.Log(numutil.ToWords(n))
-	}
-
-	for n := 1000000000; n < 1000000000000; n += 7747543654 {
-		t.Log(numutil.ToWords(n))
+	// t.SkipNow()
+	for n := 0; n < math.MaxInt64; n += ((n + 1) * -3) {
+		numutil.ToWords(n)
 	}
 }
